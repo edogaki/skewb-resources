5dce1026d83de3a9aa318201d4a7523f882f663c
edogaki <225130043+edogaki@users.noreply.github.com>
2026-07-22 12:23:02 +0800

    Add favicon

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -46,6 +46,12 @@ export const Route = createRootRoute({
                 rel: "stylesheet",
                 href: appCss,
             },
+            {
+                rel: "icon",
+                type: "image/png",
+                sizes: "96x96",
+                href: "/skewb-resources/favicon.png",
+            },
         ],
     }),
     shellComponent: RootDocument,
