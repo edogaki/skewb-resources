69839d3c6dd65971e238fc5c1368ebeb2bb12a8b
edogaki <225130043+edogaki@users.noreply.github.com>
2026-09-04 16:22:55 +0800

    Prevent duplication of 3d cube

--- src/components/Skewb3D.tsx
+++ src/components/Skewb3D.tsx
@@ -1,23 +1,31 @@
 import { TwistyPlayer } from "cubing/twisty";
-import { useEffect, useRef } from "react";
+import { useEffect, useMemo, useRef } from "react";
 import type { WCAAlg } from "#/utils/solver/alg";
 
-const player = new TwistyPlayer({
-    puzzle: "skewb",
-    background: "none",
-    controlPanel: "none",
-});
-
 export default function Skewb3D({ setupAlg }: { setupAlg: WCAAlg }) {
+    const player = useMemo(
+        () =>
+            new TwistyPlayer({
+                puzzle: "skewb",
+                background: "none",
+                controlPanel: "none",
+            }),
+        [],
+    );
     const divRef = useRef<HTMLDivElement>(null);
     useEffect(() => {
         if (!divRef.current) return;
         divRef.current.innerHTML = "";
         divRef.current.appendChild(player);
-        return () => {};
-    }, []);
+        return () => {
+            if (!divRef.current) return;
+            divRef.current.removeChild(player);
+            divRef.current.innerHTML = "";
+        };
+    }, [player]);
     useEffect(() => {
+        if (!divRef.current) return;
         player.experimentalSetupAlg = setupAlg.toString();
-    }, [setupAlg]);
+    }, [setupAlg, player]);
     return <div ref={divRef}></div>;
 }
